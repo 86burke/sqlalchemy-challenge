5d39d6850e64fbb0953443b046875d5a2a746fbf
86burke <86738793+86burke@users.noreply.github.com>
2021-09-28 18:17:21 -0500

    connected session with DB

--- app.py
+++ app.py
@@ -2,8 +2,44 @@
  "cells": [
   {
    "cell_type": "code",
+   "execution_count": 1,
+   "id": "f72cdb9c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#import\n",
+    "from flask import Flask, jsonify\n",
+    "import numpy as np\n",
+    "import pandas as pd\n",
+    "import sqlalchemy\n",
+    "import datetime as dt\n",
+    "from sqlalchemy.ext.automap import automap_base\n",
+    "from sqlalchemy.orm import Session\n",
+    "from sqlalchemy import create_engine, func"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "6fef35db",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Engines and Database\n",
+    "engine = create_engine(\"sqlite:///Resources/hawaii.sqlite\")\n",
+    "Base = automap_base()\n",
+    "Base.prepare(engine, reflect = True)\n",
+    "\n",
+    "Station = Base.classes.station\n",
+    "Measurement = Base.classes.measurement\n",
+    "\n",
+    "session = Session(engine)"
+   ]
+  },
+  {
+   "cell_type": "code",
    "execution_count": null,
-   "id": "7f7bbb23",
+   "id": "2db27cdf",
    "metadata": {},
    "outputs": [],
    "source": []
